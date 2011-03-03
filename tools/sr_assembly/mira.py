c91cfb5f78475440e218cdc9910768eb971dbb4e
peterjc <p.j.a.cock@googlemail.com>
2011-03-03 13:49:47 +0000

    Capture MIRA contig summary table as tabular file

--- tools/sr_assembly/mira.py
+++ tools/sr_assembly/mira.py
@@ -11,8 +11,27 @@ def stop_err(msg, err=1):
     sys.stderr.write(msg+"\n")
     sys.exit(err)
 
+def tcs_to_tabular(old, new):
+    in_handle = open(old, "rU")
+    out_handle = open(new, "w")
+    assert in_handle.readline() == "#TCS V1.0\n"
+    assert in_handle.readline() == "#\n"
+    assert in_handle.readline() == "# contig name          padPos  upadPos | B  Q | tcov covA covC covG covT cov* | qA qC qG qT q* |  S | Tags\n"
+    assert in_handle.readline() == "#\n"
+    out_handle.write("#%s\n" % "\t".join(["contig", "pasPos", "upadPos", "B", "Q",
+                                         "tcov", "covA", "covC", "covG", "covT", "cov*",
+                                         "qA", "qC", "qG", "qT", "q*", "S", "Tags"]))
+    for line in in_handle:
+        parts = line.rstrip("\n").split(None,22)
+        assert parts[3] == parts[6] == parts[13] == parts[19] == parts[21] == "|"
+        wanted = parts[:3] + parts[4:6]+parts[7:13]+parts[14:19]+parts[20:21]+parts[22:]
+        out_handle.write("%s\n" % "\t".join(wanted))
+    out_handle.close()
+    in_handle.close()
+
 def collect_output(temp, name):
     n3 = (temp, name, name, name)
+    tcs_to_tabular("%s/%s_assembly/%s_d_results/%s_out.tcs" % n3, out_tcs)
     for old, new in [("%s/%s_assembly/%s_d_results/%s_out.unpadded.fasta" % n3, out_fasta),
                      ("%s/%s_assembly/%s_d_results/%s_out.unpadded.fasta.qual" % n3, out_qual),
                      ("%s/%s_assembly/%s_d_results/%s_out.wig" % n3, out_wig),
@@ -30,11 +49,11 @@ def clean_up(temp, name):
 
 #TODO - Run MIRA in /tmp or a configurable directory?
 temp = "."
-name, out_fasta, out_qual, out_ace, out_caf, out_wig, out_log = sys.argv[1:8]
+name, out_fasta, out_qual, out_tcs, out_ace, out_caf, out_wig, out_log = sys.argv[1:9]
 start_time = time.time()
 try:
-    cmd = " ".join(sys.argv[8:])
-    child = subprocess.Popen(sys.argv[8:],
+    cmd = " ".join(sys.argv[9:])
+    child = subprocess.Popen(sys.argv[9:],
                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 except Exception, err:
     sys.stderr.write("Error invoking command:\n%s\n\n%s\n" % (cmd, err))
