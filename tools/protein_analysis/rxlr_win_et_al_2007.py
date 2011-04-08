82dee5df2e06571cfb04f8461e7eddf622ff48a2
peterjc <p.j.a.cock@googlemail.com>
2011-04-08 11:35:47 +0100

    Speed up RXLR tool by only calling SignalP on potential matches

--- tools/protein_analysis/rxlr_win_et_al_2007.py
+++ tools/protein_analysis/rxlr_win_et_al_2007.py
@@ -29,14 +29,23 @@ signalp_input_file = tabular_file + ".fasta.tmp"
 signalp_output_file = tabular_file + ".tabular.tmp"
 signalp_trunc = 70
 
+
 #Prepare short list of candidates containing RXLR to pass to SignalP
+count = 0
+total = 0
 handle = open(signalp_input_file, "w")
 for title, seq in fasta_iterator(fasta_file):
+   total += 1
    name = title.split(None,1)[0]
-   if True:
+   rxlr_match = re_rxlr.search(seq.upper())
+   if rxlr_match and rxlr_match.start() + 1 <= 60: #60 using 1-based counting
+      #This is a potential RXLR, depending on the SignalP results.
       #Might as well truncate the sequence now, makes the temp file smaller
       handle.write(">%s (truncated)\n%s\n" % (name, seq[:signalp_trunc]))
+      count += 1
 handle.close()
+#print "Running SignalP on %i/%i potentials." % (count, total)
+
 
 #Run SignalP (using our wrapper script to get multi-core support etc)
 signalp_script = os.path.join(os.path.split(sys.argv[0])[0], "signalp3.py")
@@ -47,43 +56,59 @@ return_code = os.system(cmd)
 if return_code:
    stop_err("Error %i from SignalP:\n%s" % (return_code, cmd))
 
+
 def parse_signalp(filename):
     """Parse SignalP output, yield tuples of ID, HMM_Sprob_score and NN predicted signal peptide length.
-
-    Note I leave the HHM probability as a string for loss-less output.
 
     For signal peptide length we use NN_Ymax_pos (minus one).
     """
     handle = open(filename)
     line = handle.readline()
     assert line.startswith("#ID\t"), line
-    #Tabular
     for line in handle:
         parts = line.rstrip("\t").split("\t")
         assert len(parts)==20, repr(line)
-        yield parts[0], parts[18], int(parts[5])-1
+        yield parts[0], float(parts[18]), int(parts[5])-1
     handle.close()
 
+
+#Parse SignalP results and apply the strict RXLR criteria
 count = 0
 total = 0
 handle = open(tabular_file, "w")
 handle.write("#ID\tRXLR\n")
-for (title, seq),(sp_id, sp_hmm_score, sp_nn_len) \
-in zip(fasta_iterator(fasta_file), parse_signalp(signalp_output_file)):
-    assert title.split(None,1)[0] == sp_id
+signalp_results = parse_signalp(signalp_output_file)
+for title, seq in fasta_iterator(fasta_file):
     total += 1
     rxlr = "N"
-    if float(sp_hmm_score) > 0.9 and 10 <= sp_nn_len <= 30:
-       rxlr_match = re_rxlr.search(seq[sp_nn_len:].upper())
-       if rxlr_match:
-           rxlr_pos = sp_nn_len + rxlr_match.start() + 1 #one based counting
-           if 30 <= rxlr_pos <= 60:
-               rxlr = "Y"
-               count += 1
-    handle.write("%s\t%s\n" % (sp_id,  rxlr))
+    name = title.split(None,1)[0]
+    rxlr_match = re_rxlr.search(seq.upper())
+    if rxlr_match and rxlr_match.start() + 1 <= 60: #60 using 1-based counting
+       del rxlr_match
+       #This was the criteria for calling SignalP,
+       #so it will be in the SignalP results.
+       sp_id, sp_hmm_score, sp_nn_len = signalp_results.next()
+       assert name == sp_id, "%s vs %s" % (name, sp_id)
+       if sp_hmm_score > 0.9 and 10 <= sp_nn_len <= 30:
+          rxlr_match = re_rxlr.search(seq[sp_nn_len:].upper())
+          if rxlr_match:
+              rxlr_pos = sp_nn_len + rxlr_match.start() + 1 #1-based counting
+              if 30 <= rxlr_pos <= 60:
+                  rxlr = "Y"
+                  count += 1
+    handle.write("%s\t%s\n" % (name,  rxlr))
 handle.close()
+
+#Check the iterator is finished
+try:
+   signalp_results.next()
+   assert False, "Unexpected data in SignalP output"
+except StopIteration:
+   pass
 
 #Cleanup
 os.remove(signalp_input_file)
 os.remove(signalp_output_file)
+
+#Short summary to stdout for Galaxy's info display
 print "%i out of %i have RXLR motif" % (count, total)
