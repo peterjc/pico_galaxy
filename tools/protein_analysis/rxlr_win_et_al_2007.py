ee889f0ff4fbbc419cb6008b41f8fe6c598e6e82
peterjc <p.j.a.cock@googlemail.com>
2011-04-08 14:15:22 +0100

    Fixed 3 space indentation. Added a reference

--- tools/protein_analysis/rxlr_win_et_al_2007.py
+++ tools/protein_analysis/rxlr_win_et_al_2007.py
@@ -22,7 +22,7 @@ from seq_analysis_utils import stop_err, fasta_iterator
 re_rxlr = re.compile("R.LR")
 
 if len(sys.argv) != 4:
-   stop_err("Requires three arguments: protein FASTA filename, threads, and output filename")
+    stop_err("Requires three arguments: protein FASTA filename, threads, and output filename")
 
 fasta_file, threads, tabular_file = sys.argv[1:]
 signalp_input_file = tabular_file + ".fasta.tmp"
@@ -35,14 +35,14 @@ count = 0
 total = 0
 handle = open(signalp_input_file, "w")
 for title, seq in fasta_iterator(fasta_file):
-   total += 1
-   name = title.split(None,1)[0]
-   rxlr_match = re_rxlr.search(seq.upper())
-   if rxlr_match and rxlr_match.start() + 1 <= 60: #60 using 1-based counting
-      #This is a potential RXLR, depending on the SignalP results.
-      #Might as well truncate the sequence now, makes the temp file smaller
-      handle.write(">%s (truncated)\n%s\n" % (name, seq[:signalp_trunc]))
-      count += 1
+    total += 1
+    name = title.split(None,1)[0]
+    match = re_rxlr.search(seq.upper())
+    if match and match.start() + 1 <= 60: #60 using 1-based counting
+        #This is a potential RXLR, depending on the SignalP results.
+        #Might as well truncate the sequence now, makes the temp file smaller
+        handle.write(">%s (truncated)\n%s\n" % (name, seq[:signalp_trunc]))
+        count += 1
 handle.close()
 #print "Running SignalP on %i/%i potentials." % (count, total)
 
@@ -50,11 +50,11 @@ handle.close()
 #Run SignalP (using our wrapper script to get multi-core support etc)
 signalp_script = os.path.join(os.path.split(sys.argv[0])[0], "signalp3.py")
 if not os.path.isfile(signalp_script):
-   stop_err("Error - missing signalp3.py script")
+    stop_err("Error - missing signalp3.py script")
 cmd = "python %s euk %i %s %s %s" % (signalp_script, signalp_trunc, threads, signalp_input_file, signalp_output_file)
 return_code = os.system(cmd)
 if return_code:
-   stop_err("Error %i from SignalP:\n%s" % (return_code, cmd))
+    stop_err("Error %i from SignalP:\n%s" % (return_code, cmd))
 
 
 def parse_signalp(filename):
@@ -84,27 +84,27 @@ for title, seq in fasta_iterator(fasta_file):
     name = title.split(None,1)[0]
     rxlr_match = re_rxlr.search(seq.upper())
     if rxlr_match and rxlr_match.start() + 1 <= 60: #60 using 1-based counting
-       del rxlr_match
-       #This was the criteria for calling SignalP,
-       #so it will be in the SignalP results.
-       sp_id, sp_hmm_score, sp_nn_len = signalp_results.next()
-       assert name == sp_id, "%s vs %s" % (name, sp_id)
-       if sp_hmm_score > 0.9 and 10 <= sp_nn_len <= 30:
-          rxlr_match = re_rxlr.search(seq[sp_nn_len:].upper())
-          if rxlr_match:
-              rxlr_pos = sp_nn_len + rxlr_match.start() + 1 #1-based counting
-              if 30 <= rxlr_pos <= 60:
-                  rxlr = "Y"
-                  count += 1
+        del rxlr_match
+        #This was the criteria for calling SignalP,
+        #so it will be in the SignalP results.
+        sp_id, sp_hmm_score, sp_nn_len = signalp_results.next()
+        assert name == sp_id, "%s vs %s" % (name, sp_id)
+        if sp_hmm_score > 0.9 and 10 <= sp_nn_len <= 30:
+           rxlr_match = re_rxlr.search(seq[sp_nn_len:].upper())
+           if rxlr_match:
+               rxlr_pos = sp_nn_len + rxlr_match.start() + 1 #1-based counting
+               if 30 <= rxlr_pos <= 60:
+                   rxlr = "Y"
+                   count += 1
     handle.write("%s\t%s\n" % (name,  rxlr))
 handle.close()
 
 #Check the iterator is finished
 try:
-   signalp_results.next()
-   assert False, "Unexpected data in SignalP output"
+    signalp_results.next()
+    assert False, "Unexpected data in SignalP output"
 except StopIteration:
-   pass
+    pass
 
 #Cleanup
 os.remove(signalp_input_file)
