fa0df494bf8bf85502d49ec68f882e9a6b83ee71
peterjc <p.j.a.cock@googlemail.com>
2014-08-12 14:31:38 +0100

    Correct copyright date for sample_seqs.py

--- tools/sample_seqs/sample_seqs.py
+++ tools/sample_seqs/sample_seqs.py
@@ -9,7 +9,7 @@ Cock et al 2009. Biopython: freely available Python tools for computational
 molecular biology and bioinformatics. Bioinformatics 25(11) 1422-3.
 http://dx.doi.org/10.1093/bioinformatics/btp163 pmid:19304878.
 
-This script is copyright 2010-2013 by Peter Cock, The James Hutton Institute
+This script is copyright 2014 by Peter Cock, The James Hutton Institute
 (formerly the Scottish Crop Research Institute, SCRI), UK. All rights reserved.
 See accompanying text file for licence details (MIT license).
 
