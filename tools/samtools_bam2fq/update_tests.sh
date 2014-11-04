aec2057585fc46a58cbec05a9de50a0ae242b691
peterjc <p.j.a.cock@googlemail.com>
2014-11-04 09:34:13 +0000

    Test file with samtools bam2fq -n option (no suffices)

--- tools/samtools_bam2fq/update_tests.sh
+++ tools/samtools_bam2fq/update_tests.sh
@@ -12,9 +12,11 @@ fi
 
 cd test-data
 
-echo sam_spec_padded.bam2fq.fastq
+echo "sam_spec_padded.bam2fq.fastq (defaults)"
 samtools bam2fq sam_spec_padded.bam > sam_spec_padded.bam2fq.fastq
 # Should give idential output to:
 # $ samtools bam2fq sam_spec_padded.sam > sam_spec_padded.sam2fq.fastq
 # $ samtools bam2fq sam_spec_padded.depad.bam > sam_spec_padded.depad.bam2fq.fastq
 
+echo "sam_spec_padded.bam2fq_no_suf.fastq (without suffices)"
+samtools bam2fq -n sam_spec_padded.bam > sam_spec_padded.bam2fq_no_suf.fastq
